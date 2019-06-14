43fb7cab3c20f11f757347c25db99633b0cca249
Yeni <yen.diaz@hotmail.com>
2019-06-14 13:50:45 -0400

    APIDAL

--- RouteFinder/Models/RouteAPIDAL.cs
+++ RouteFinder/Models/RouteAPIDAL.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 
 namespace RouteFinder.Models
 {
     public class RouteAPIDAL
     {
+        public static string APICall(string URL)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+
+            return APIText;
+        }
+
+        public static string Cooridnates (string startDestination,string endDestination,string avoidCoorinates)
+        {
+            string URL = "";
+            APICall(URL);
+
+        }
     }
-}
+} 
