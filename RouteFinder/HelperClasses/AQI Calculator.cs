d7c67ddca0c21493a4c6599959e908d4e8fefd1e
Eric Holt <fatmoti@gmail.com>
2019-06-22 10:22:15 -0400

    Added validation for other pages to redirect to index. Also check if session data exist when loading routemap and use that instead.

--- RouteFinder/HelperClasses/AQI Calculator.cs
+++ RouteFinder/HelperClasses/AQI Calculator.cs
@@ -11,28 +11,8 @@ namespace RouteFinder.Models
 {
     public class AQI_Calculator
     {
-        //public enum Pollutant
-
-        //{
-        //    PM25, PM10, O3,
-        //}
 
 
-
-
-
-
-
-
-
-
-            //public  Pollutant(Pollutant pollutant, double concentration)
-            //{
-            //    if (Pollutant.PM10.equals(pollutant) || Pollutant.SO2.equals(pollutant) || Pollutant.NO2.equals(pollutant))
-            //    {
-            //        return Math.round(concentration);
-            //    }
-            //}
-        }
     }
+}
 
